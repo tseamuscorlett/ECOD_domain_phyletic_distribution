53a4a716f13cb9205020fa1b6d4f129df47d3111
tseamuscorlett <t.seamus.corlett@gmail.com>
2025-04-10 16:18:12 +0900

    Delete 2 files, modify main, rename files

--- aerobe_annotation.py
+++ aerobe_annotation.py
@@ -78,12 +78,12 @@ def parseMatrix(file_path):
 """
 
 # file_path = 'xgroup_matrix_archaea_recovered.csv'
-# file_path = 'xgroup_matrix_bacteria_recovered.csv'
+file_path = 'xgroup_matrix_bacteria_recovered.csv'
 # file_path = 'xgroup_matrix_eukaryotes_recovered.csv'
 
-# domains_dict, genomes_dict, output = parseMatrix(file_path)
-# print(len(domains_dict))
-# print(len(genomes_dict))
+domains_dict, genomes_dict, output = parseMatrix(file_path)
+print(len(domains_dict))
+print(len(genomes_dict))
 #
 # xgroups = ['3997', '1055', '136', '158', '185', '199', '2010', '239', '270', '3001', '3052', '309', '3115', '319', '3688', '374', '3754', '3896', '4004', '4019', '4022', '4028', '4048', '4076', '4110', '4126', '4160', '4237', '4295', '5084', '528', '582', '590', '6051', '6096', '640', '7513', '7551', '7553', '7567', '842', '869', '875', '876', '9']
 # xgroups = ['3997', '1055', '158', '185', '199', '374', '4237', '582', '6051', '640', '869']  # excluded widely distributed xgroups
@@ -116,33 +116,33 @@ def parseMatrix(file_path):
 Josh annotation for aerobe, anaerobe, and facultative bacteria
 """
 
-# genome_path = 'gtdb_r207_3350genomesWithO2Requirements_copy.csv'
-# genome2oxy = csv2dict(genome_path)
-# print(len(genome2oxy))  # 3315
-#
-# x2ns = csv2dict("data/xgroup2networkSize.csv")
-# print(len(x2ns))
-# xgroup397 = set(x2ns.keys())
+genome_path = 'gtdb_r207_3350genomesWithO2Requirements_copy.csv'
+genome2oxy = csv2dict(genome_path)
+print(len(genome2oxy))  # 3315
 
-# xgroup2oxy = {}
-# for xgroup in xgroup397:  # 397
-#     xgroup2oxy[xgroup] = {'aerobe': 0, 'anaerobe': 0, 'facultative': 0, 'unknown': 0}
-#     for genome in genomes_dict.keys():  # ~62,000; contains 3-letter prefix
-#         if output[genomes_dict[genome]][domains_dict[xgroup]] > 0:  # xgroup exists in this genome
-#             if genome2oxy.get(genome[3:], "N/A") == ['Aerobe']:  # does not contain 3-letter prefix
-#                 xgroup2oxy[xgroup]['aerobe'] += 1  # max 1711
-#             elif genome2oxy.get(genome[3:], "N/A") == ['Anaerobe']:
-#                 xgroup2oxy[xgroup]['anaerobe'] += 1  # max 1187
-#             elif genome2oxy.get(genome[3:], "N/A") == ['Facultative']:
-#                 xgroup2oxy[xgroup]['facultative'] += 1  # max 417
-#
-#             else:
-#                 xgroup2oxy[xgroup]['unknown'] += 1
-#
-# print(xgroup2oxy['3997'])  # 42, 22, 15, 1112
-# print(xgroup2oxy['2003'])  # 1644, 1105, 409, 59133
+x2ns = csv2dict("data/xgroup2networkSize.csv")
+print(len(x2ns))
+xgroup397 = set(x2ns.keys())
 
-# dict2csv(xgroup2oxy, 'xgroup2oxy_bacteria.csv')
+xgroup2oxy = {}
+for xgroup in xgroup397:  # 397
+    xgroup2oxy[xgroup] = {'aerobe': 0, 'anaerobe': 0, 'facultative': 0, 'unknown': 0}
+    for genome in genomes_dict.keys():  # ~62,000; contains 3-letter prefix
+        if output[genomes_dict[genome]][domains_dict[xgroup]] > 0:  # xgroup exists in this genome
+            if genome2oxy.get(genome[3:], "N/A") == ['Aerobe']:  # does not contain 3-letter prefix
+                xgroup2oxy[xgroup]['aerobe'] += 1  # max 1711
+            elif genome2oxy.get(genome[3:], "N/A") == ['Anaerobe']:
+                xgroup2oxy[xgroup]['anaerobe'] += 1  # max 1187
+            elif genome2oxy.get(genome[3:], "N/A") == ['Facultative']:
+                xgroup2oxy[xgroup]['facultative'] += 1  # max 417
+
+            else:
+                xgroup2oxy[xgroup]['unknown'] += 1
+
+print(xgroup2oxy['3997'])  # 42, 22, 15, 1112
+print(xgroup2oxy['2003'])  # 1644, 1105, 409, 59133
+
+dict2csv(xgroup2oxy, 'xgroup2oxy_bacteria.csv')
 
 
 """
@@ -185,20 +185,20 @@ genome2x instead
 """
 Eukaryotes hit distribution
 """
-file_path = 'xgroup_matrix_eukaryotes_recovered.csv'
-
-domains_dict, genomes_dict, output = parseMatrix(file_path)
-print(len(domains_dict))
-print(len(genomes_dict))
-
-xgroup2230 = domains_dict.keys()
-
-genome2x = {}
-for g in genomes_dict.keys():  # 196 genomes
-    hits = {}
-    for x in xgroup2230:
-        hits[x] = output[genomes_dict[g]][domains_dict[x]]
-    genome2x[g] = hits
-
-print(len(genome2x))
-dict2csv(genome2x, 'genome2x_euk_Full.csv')
+# file_path = 'xgroup_matrix_eukaryotes_recovered.csv'
+#
+# domains_dict, genomes_dict, output = parseMatrix(file_path)
+# print(len(domains_dict))
+# print(len(genomes_dict))
+#
+# xgroup2230 = domains_dict.keys()
+#
+# genome2x = {}
+# for g in genomes_dict.keys():  # 196 genomes
+#     hits = {}
+#     for x in xgroup2230:
+#         hits[x] = output[genomes_dict[g]][domains_dict[x]]
+#     genome2x[g] = hits
+#
+# print(len(genome2x))
+# dict2csv(genome2x, 'genome2x_euk_Full.csv')
